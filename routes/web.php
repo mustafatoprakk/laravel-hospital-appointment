d134f0a00dfe94ada085702208ae37d78bf56cbd
mustafa toprak <mmustafa.ttoprak@gmail.com>
2022-05-25 07:34:55 +0300

    user template tamamlandı

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get("/", [HomeController::class, "index"]);
 
 Route::middleware([
     'auth:sanctum',
